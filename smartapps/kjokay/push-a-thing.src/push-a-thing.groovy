d70e2b69f2deeca2cebb20439936f049658e299a
Kevin Johns <kevin.johns@gmail.com>
2016-11-18 21:17:32 -0700

    first commit

---
+++ smartapps/kjokay/push-a-thing.src/push-a-thing.groovy
@@ -0,0 +1,43 @@
+/**
+ *  push a thing
+ *
+ *  Copyright 2016 Kevin Johns
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+preferences {
+    section("Which door?") {
+        input "door", "capability.contactSensor", required: true,
+              title: "Which door?"
+    }
+    section("Send Push Notification?") {
+        input "sendPush", "bool", required: false,
+              title: "Send Push Notification when Opened?"
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    initialize()
+}
+
+def initialize() {
+    subscribe(door, "contact.open", doorOpenHandler)
+}
+
+def doorOpenHandler(evt) {
+    if (sendPush) {
+        sendPush("The ${door.displayName} is open!")
+    }
+}
